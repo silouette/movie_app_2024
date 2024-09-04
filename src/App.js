2aeebbe9943a8000103822c26866f3c523edcc7f
silouette <domenica0@naver.com>
2024-09-04 23:08:45 +0900

    component in component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
+import Potato from './Potato';
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
+      <Potato />
     </div>
   );
 }
---
+++ src/Potato.js
@@ -0,0 +1,8 @@
+// React가 JSX를 이해할 수 있게 됨
+import React from 'react';
+
+function Potato() {
+    return <h3>I love potato</h3>;
+}
+
+export default Potato;
