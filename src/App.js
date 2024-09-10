9829603196d16ea63e6700c01cb56fcb9729c5a9
silouette <domenica0@naver.com>
2024-09-10 20:08:10 +0900

    '테스트'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 // import logo from './logo.svg';
 // import './App.css';
 import React from 'react';
-import Potato from './Potato';
+// import Potato from './Potato';
+
+function Potato() {
+  return <h1>I like potato</h1>
+}
 
 function App() {
   return (
