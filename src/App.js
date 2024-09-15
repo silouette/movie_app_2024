d1d73caae625d1e6bb0adb26f55ea62e18047e1c
DESKTOP-A6ONK9D\domen <domenica0@naver.com>
2024-09-16 07:42:00 +0900

    map() 함수로 많은 컴포넌트 사용

--- src/App.js
+++ src/App.js
@@ -3,18 +3,35 @@
 import React from 'react';
 // import Potato from './Potato';
 
-function Food({ fav }) {
-  return <h1>I like {fav}</h1>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} />
+    </div>
+  )
 }
+
+const foodILike = [
+  {
+    id: 1,
+    name: 'Kimchi',
+    image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
+  },
+  {
+    id: 2,
+    name: 'Samgyeopsal',
+    image: 'https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSc07624.jpg',
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food fav="kimchi" something={true} papapapa={['hello', 1, 2, 3, 4, true]} />
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chukumi" />
+      {foodILike.map(dish => (
+        /* console 오류 막기 위해서 컴포넌트마다 고유 key값 설정해서 전달 */
+        <Food name={dish.name} picture={dish.image} key={dish.id} />
+      ))}
     </div>
   );
 }
