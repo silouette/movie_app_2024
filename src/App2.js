bc5f7a1306842c44cc5e530a30957abb9b6936b8
silouette <domenica0@naver.com>
2024-09-30 23:10:50 +0900

    구조 분해 할당으로 movies 키의 데이터들 받아오는 코드(축약 형태로 줄이기)

--- src/App2.js
+++ src/App2.js
@@ -11,14 +11,19 @@ class App extends React.Component {
     getMovies = async () => {
         // axios.get()의 실행을 기다리도록 await 추가
         // const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
-        
+
         // ES6의 구조 분해 할당으로 movies 키의 데이터들 받아옴
         const {
             data: {
                 data: { movies }
             }
         } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
-        console.log(movies);
+        // console.log(movies);
+
+        // 구조 분해 할당으로 얻은 영화 데이터 setState
+        // this.setState({ movies: movies })
+        // ES6에서는 객체의 키와 대입할 변수의 이름이 같다면 코드를 축약 가능하다. (아래는 축약 형태로 코드를 바꾼 것)
+        this.setState({ movies });
     };
 
     componentDidMount() {
