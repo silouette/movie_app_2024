2de38c520cca4864b19ef03d7b080a887303966b
silouette <domenica0@naver.com>
2024-09-30 09:04:54 +0900

    화면로딩 추가

--- src/App2.js
+++ src/App2.js
@@ -1,48 +1,21 @@
 import React from 'react';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     state = {
-        count: 0
+        isLoading: true,
+        movies: []
     };
 
     componentDidMount() {
-        console.log('component rendered');
+        // 영화 데이터 로딩
+        setTimeout(() => {
+            this.setState({ isLoading: false })
+        }, 6000);
     }
-
-    componentDidUpdate() {
-        console.log('I just updated');
-    }
-
-    componentWillUnmount() {
-        console.log('Goodbye, cruel world');
-    }
-
-    add = () => {
-        console.log('add');
-        this.setState(current => ({
-            count: current.count + 1    
-        }));
-    };
-
-    minus = () => {
-        console.log('minus');
-        this.setState(current => ({
-            count: current.count - 1
-        }));
-    };
 
     render() {
-        return (
-            <div>
-                <h1>The number is: {this.state.count}</h1>
-                <button onClick={this.add}>Add</button>
-                <button onClick={this.minus}>Minus</button>
-            </div>
-        )
+        const { isLoading } = this.state;
+        return <div>{isLoading ? 'Loading...' : 'We are ready'} </div>;
     }
 }
 
