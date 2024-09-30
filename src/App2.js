eb515ed0701b322a48b504992486ab8caf532860
silouette <domenica0@naver.com>
2024-09-30 13:48:17 +0900

    구조 분해 할당으로 movies 키 값 받아오기

--- src/App2.js
+++ src/App2.js
@@ -10,7 +10,15 @@ class App extends React.Component {
     // 영화 데이터 로딩(getMovies() 함수는 시간이 필요하기에 async 추가)
     getMovies = async () => {
         // axios.get()의 실행을 기다리도록 await 추가
-        const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+        // const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+        
+        // ES6의 구조 분해 할당으로 movies 키의 데이터들 받아옴
+        const {
+            data: {
+                data: { movies }
+            }
+        } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+        console.log(movies);
     };
 
     componentDidMount() {
