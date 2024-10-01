3d432f0c058de3a8023e9c469b360b99ee6ad8b0
silouette <domenica0@naver.com>
2024-10-01 14:16:38 +0900

    영화 포스터 이미지 추가

--- src/Movie.js
+++ src/Movie.js
@@ -3,10 +3,13 @@ import PropTypes from 'prop-types';
 
 function Movie({ id, title, year, summary, poster }) {
     return (
-        <div class="movie__data">
-            <h3 class="movie__title">{title}</h3>
-            <h5 class="movie__year">{year}</h5>
-            <p class="movie__summary">{summary}</p>
+        <div class="movie">
+            <img src={poster} alt={title} title={title} />
+            <div class="movie__data">
+                <h3 class="movie__title">{title}</h3>
+                <h5 class="movie__year">{year}</h5>
+                <p class="movie__summary">{summary}</p>
+            </div>
         </div>
     );
 }
