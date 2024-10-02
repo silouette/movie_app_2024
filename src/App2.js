70f40648d11b34a0f0c3441ce9103f5f396f1d4f
silouette <domenica0@naver.com>
2024-10-02 14:45:48 +0900

    genres 배열 map() 함수 이용 출력

--- src/App2.js
+++ src/App2.js
@@ -34,13 +34,13 @@ class App extends React.Component {
 
     render() {
         const { isLoading, movies } = this.state;
-        return <section class="container">
+        return <section className="container">
             {isLoading ? (
-                <div class="loader">
-                    <span class="loader__text">Loading...</span>
+                <div className="loader">
+                    <span className="loader__text">Loading...</span>
                 </div>
             ) : (
-                <div class="movies">
+                <div className="movies">
                     {movies.map(movie => (
                         <Movie 
                             key={movie.id}
@@ -49,6 +49,7 @@ class App extends React.Component {
                             title={movie.title}
                             summary={movie.summary}
                             poster={movie.medium_cover_image}
+                            genres={movie.genres}
                         />
                     ))}     
                 </div>          
--- src/Movie.js
+++ src/Movie.js
@@ -2,14 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-function Movie({ title, year, summary, poster }) {
+function Movie({ title, year, summary, poster, genres }) {
     return (
-        <div class="movie">
+        <div className="movie">
             <img src={poster} alt={title} title={title} />
-            <div class="movie__data">
-                <h3 class="movie__title">{title}</h3>
-                <h5 class="movie__year">{year}</h5>
-                <p class="movie__summary">{summary}</p>
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="movie__genres">
+                    {/* genres props가 배열이므로 map() 함수 사용가능 */}
+                    {genres.map((genre) => {
+                        return <li className="movie__genre">{genre}</li>;
+                    })}
+                </ul>
+                <p className="movie__summary">{summary}</p>
             </div>
         </div>
     );
@@ -19,7 +25,8 @@ Movie.propTypes = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
