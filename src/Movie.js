78316c4d734b7202a4c37cd888b309275e13abbe
silouette <domenica0@naver.com>
2024-10-03 12:04:35 +0900

    시놉시스 180자 제한 적용

--- src/Movie.js
+++ src/Movie.js
@@ -15,7 +15,7 @@ function Movie({ title, year, summary, poster, genres }) {
                         return <li key={index} className="movie__genre">{genre}</li>;
                     })}
                 </ul>
-                <p className="movie__summary">{summary}</p>
+                <p className="movie__summary">{summary.slice(0, 180)}...</p>
             </div>
         </div>
     );
