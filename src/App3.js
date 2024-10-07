6b2044e643257ec2f74367998f3877393026201c
silouette <domenica0@naver.com>
2024-10-07 10:11:51 +0900

    About 컴포넌트 라우터 설정

--- src/App3.js
+++ src/App3.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Home from './routes/Home';
 import './App.css';
+import { HashRouter, Route } from 'react-router-dom';
+import About from './routes/About';
 
 function App() {
-    return <Home />;
+    return (
+        // HashRouter가 적용되면 앱이 실행되는 주소에 '#/'이 붙게 된다.
+        // Route에는 2가지 props를 전달할 수 있다. -> 1. URL을 위한 path props, 2. URL에 맞는 컴포넌트를 불러주기 위한 component props.
+        <HashRouter>
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/about" component={About} />
+        </HashRouter>
+    );
 }
 
 export default App;
