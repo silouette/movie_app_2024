332c08c9f0f6ab4999fb4f95ca1dce729c867c67
silouette <domenica0@naver.com>
2024-10-10 17:43:01 +0900

    About style 수정, Navigation style CSS 파일 추가

--- src/App3.js
+++ src/App3.js
@@ -9,6 +9,7 @@ function App() {
     return (
         // HashRouter가 적용되면 앱이 실행되는 주소에 '#/'이 붙게 된다.
         // Route에는 2가지 props를 전달할 수 있다. -> 1. URL을 위한 path props, 2. URL에 맞는 컴포넌트를 불러주기 위한 component props.
+        // Link(Navigation 컴포넌트에 존재), Router 컴포넌트는 반드시 HashRouter 안에 포함되어야 한다.
         <HashRouter>
             <Navigation />
             <Route path="/" exact={true} component={Home} />
