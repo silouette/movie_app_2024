51fb21ebf485dd9b6776a7160c2b952dc9ac7a48
silouette <domenica0@naver.com>
2024-10-10 16:44:19 +0900

    Navigation 컴포넌트 추가

--- src/App3.js
+++ src/App3.js
@@ -3,12 +3,14 @@ import Home from './routes/Home';
 import './App.css';
 import { HashRouter, Route } from 'react-router-dom';
 import About from './routes/About';
+import Navigation from './components/Navigation';
 
 function App() {
     return (
         // HashRouter가 적용되면 앱이 실행되는 주소에 '#/'이 붙게 된다.
         // Route에는 2가지 props를 전달할 수 있다. -> 1. URL을 위한 path props, 2. URL에 맞는 컴포넌트를 불러주기 위한 component props.
         <HashRouter>
+            <Navigation />
             <Route path="/" exact={true} component={Home} />
             <Route path="/about" component={About} />
         </HashRouter>
